44e418fdda6a149eb8de668e52569aa0c3c6697f
Raphael Vitse <raphaelv@MacBook-Pro-de-raphael.local>
2024-11-12 14:04:10 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+import logo from "./img/";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "./img/logo-netflix.png";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src={logo} alt="logo neflix" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
